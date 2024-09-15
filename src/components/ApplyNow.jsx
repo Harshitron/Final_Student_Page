9018211654e173f7273ed185f48bcc89f748dd34
Harshit Sachdeva <harshitsachdeva291104@gmail.com>
2024-09-16 00:52:56 +0530

    Final Commit(Hopefully)

--- src/components/ApplyNow.jsx
+++ src/components/ApplyNow.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 const ApplyNow = () => {
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState({ 
     // Personal Details
     name: '',
     fatherName: '',
